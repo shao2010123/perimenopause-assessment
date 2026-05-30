91d5903a23c41f920c82c5a1440d04f97ff66815
shao2010123 <shaofan2018@gmail.com>
2026-05-30 16:25:35 +0800

    Delete perimenopause-assessment-deploy directory

--- perimenopause-assessment-deploy/src/utils/phoneFormat.js
+++
@@ -1,6 +0,0 @@
-export function maskPhone(phone) {
-  if (!phone) return '';
-  const digits = String(phone).replace(/\s+/g, '');
-  if (digits.length < 7) return digits;
-  return `${digits.slice(0, 3)}****${digits.slice(-4)}`;
-}
